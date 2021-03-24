e2dd0b20a23a1ac01998de5f48a0860995c2f877
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-03-24 14:23:41 +0000

    log and continue on errors

--- imgs2vids.sh
+++ imgs2vids.sh
@@ -8,19 +8,30 @@ function convert_images () {
     mkdir -p /tmp/asjpg
     rm -f /tmp/asjpg/*
     cd "$1"/"$2"
-    mogrify -path /tmp/asjpg -format jpg -depth 8 -evaluate multiply 4 *.tiff
+    mogrify \
+        -depth 8 \
+        -evaluate multiply 4 \
+        -format jpg \
+        -path /tmp/asjpg \
+        *.tiff \
+        || echo "ERROR: mogrify failure --" "$1"/"$2"
 }
 
 function render_video () {
     mkdir -p "$1"
     cd /tmp/asjpg
-    ffmpeg -y -hide_banner -loglevel error -r 2 -i "$2"_t%d_p0.ome.jpg -c:v libx264 -vf "fps=25,format=yuv420p" "$1"/"$2".mp4
+    ffmpeg -y -hide_banner -loglevel error \
+        -i "$2"_t%d_p0.ome.jpg \
+        -r 2 \
+        -c:v libx264 \
+        -vf "fps=25,format=yuv420p" \
+        "$1"/"$2".mp4 \
+        || echo "ERROR: ffmpeg failure --" "$1"/"$2"
     rm -rf /tmp/asjpg
 }
 
 cd "$DIR"
 for subdir in */; do
-    echo ${subdir%/}
     convert_images "$DIR"    "${subdir%/}"
     render_video   "$OUTPUT" "${subdir%/}"
 done
