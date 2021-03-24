ddba583e0a43893856840408d94e2a2ff5e55166
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-03-24 13:21:57 +0000

    extract functions

--- imgs2vids.sh
+++ imgs2vids.sh
@@ -2,9 +2,19 @@
 set -euo pipefail
 
 NAME="63x_2020-10-21_141235"
-cd "/media/hff/My Passport/Eva K/191021_C2_DIV2_cort_neurons_F4/${NAME}"
-mkdir -p /tmp/asjpg
-rm /tmp/asjpg/*
-mogrify -path /tmp/asjpg -format jpg -depth 8 -evaluate multiply 4 *.tiff
-cd /tmp/asjpg
-ffmpeg -y -r 2 -i ${NAME}_t%d_p0.ome.jpg -c:v libx264 -vf "fps=25,format=yuv420p" /tmp/$NAME.mp4
+
+function convert_images () {
+    mkdir -p /tmp/asjpg
+    rm /tmp/asjpg/*
+    cd "/media/hff/My Passport/Eva K/191021_C2_DIV2_cort_neurons_F4/${1}"
+    mogrify -path /tmp/asjpg -format jpg -depth 8 -evaluate multiply 4 *.tiff
+}
+
+function render_video () {
+    mkdir -p /tmp/eva_calcium_vids
+    cd /tmp/asjpg
+    ffmpeg -y -r 2 -i ${1}_t%d_p0.ome.jpg -c:v libx264 -vf "fps=25,format=yuv420p" /tmp/eva_calcium_vids/${1}.mp4
+}
+
+convert_images $NAME
+render_video $NAME
