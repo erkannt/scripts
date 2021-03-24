0e71d5005ecbbe581270e41df55f954e07027e1f
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-03-24 13:37:02 +0000

    extract topdir variable and quieten ffmpeg

--- imgs2vids.sh
+++ imgs2vids.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 set -euo pipefail
 
-NAME="63x_2020-10-21_141235"
+DIR="/media/hff/My Passport/Eva K/191021_C2_DIV2_cort_neurons_F4/"
+SUBDIR="63x_2020-10-21_141235"
 
 function convert_images () {
     mkdir -p /tmp/asjpg
-    rm /tmp/asjpg/*
-    cd "/media/hff/My Passport/Eva K/191021_C2_DIV2_cort_neurons_F4/${1}"
+    rm -f /tmp/asjpg/*
+    cd "$1"/"$2"
     mogrify -path /tmp/asjpg -format jpg -depth 8 -evaluate multiply 4 *.tiff
 }
 
 function render_video () {
     mkdir -p /tmp/eva_calcium_vids
     cd /tmp/asjpg
-    ffmpeg -y -r 2 -i ${1}_t%d_p0.ome.jpg -c:v libx264 -vf "fps=25,format=yuv420p" /tmp/eva_calcium_vids/${1}.mp4
+    ffmpeg -y -hide_banner -loglevel error -r 2 -i ${1}_t%d_p0.ome.jpg -c:v libx264 -vf "fps=25,format=yuv420p" /tmp/eva_calcium_vids/${1}.mp4
 }
 
-convert_images $NAME
-render_video $NAME
+convert_images "$DIR" "$SUBDIR"
+render_video "$SUBDIR"
