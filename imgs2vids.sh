1174900026c5b40cc611eeccc473fd45b2795328
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-03-24 13:38:15 +0000

    improve with shellcheck

--- imgs2vids.sh
+++ imgs2vids.sh
@@ -8,13 +8,13 @@ function convert_images () {
     mkdir -p /tmp/asjpg
     rm -f /tmp/asjpg/*
     cd "$1"/"$2"
-    mogrify -path /tmp/asjpg -format jpg -depth 8 -evaluate multiply 4 *.tiff
+    mogrify -path /tmp/asjpg -format jpg -depth 8 -evaluate multiply 4 -- *.tiff
 }
 
 function render_video () {
     mkdir -p /tmp/eva_calcium_vids
     cd /tmp/asjpg
-    ffmpeg -y -hide_banner -loglevel error -r 2 -i ${1}_t%d_p0.ome.jpg -c:v libx264 -vf "fps=25,format=yuv420p" /tmp/eva_calcium_vids/${1}.mp4
+    ffmpeg -y -hide_banner -loglevel error -r 2 -i "$1"_t%d_p0.ome.jpg -c:v libx264 -vf "fps=25,format=yuv420p" /tmp/eva_calcium_vids/"$1".mp4
 }
 
 convert_images "$DIR" "$SUBDIR"
