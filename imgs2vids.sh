42993c24e474768bf6472f03007435f0e6d2b844
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-03-24 13:46:26 +0000

    tidy up after ourselves

--- imgs2vids.sh
+++ imgs2vids.sh
@@ -2,7 +2,6 @@
 set -euo pipefail
 
 DIR="/media/hff/My Passport/Eva K/191021_C2_DIV2_cort_neurons_F4/"
-SUBDIR="63x_2020-10-21_141235"
 
 function convert_images () {
     mkdir -p /tmp/asjpg
@@ -15,6 +14,7 @@ function render_video () {
     mkdir -p /tmp/eva_calcium_vids
     cd /tmp/asjpg
     ffmpeg -y -hide_banner -loglevel error -r 2 -i "$1"_t%d_p0.ome.jpg -c:v libx264 -vf "fps=25,format=yuv420p" /tmp/eva_calcium_vids/"$1".mp4
+    rm -rf /tmp/asjpg
 }
 
 cd "$DIR"
