affd562381897abf294ee650065682d87b13f867
Daniel Haarhoff <d.haarhoff@elifesciences.org>
2021-03-24 13:45:17 +0000

    run over all subdirs

--- imgs2vids.sh
+++ imgs2vids.sh
@@ -17,5 +17,10 @@ function render_video () {
     ffmpeg -y -hide_banner -loglevel error -r 2 -i "$1"_t%d_p0.ome.jpg -c:v libx264 -vf "fps=25,format=yuv420p" /tmp/eva_calcium_vids/"$1".mp4
 }
 
-convert_images "$DIR" "$SUBDIR"
-render_video "$SUBDIR"
+cd "$DIR"
+for subdir in */; do
+    echo ${subdir%/}
+    convert_images "$DIR" "${subdir%/}"
+    render_video "${subdir%/}"
+done
+
